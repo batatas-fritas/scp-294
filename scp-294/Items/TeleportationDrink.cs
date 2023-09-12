7ffc61540591871bfa1116d504c9e64f6543b8c4
batatas-fritas <francisco.miguel.sousa@gmail.com>
2023-09-12 16:09:58 +0100

    Removed unused references

--- scp-294/Items/TeleportationDrink.cs
+++ scp-294/Items/TeleportationDrink.cs
@@ -1,5 +1,4 @@
-﻿using Exiled.API.Enums;
-using Exiled.API.Features.Attributes;
+﻿using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs.Player;
